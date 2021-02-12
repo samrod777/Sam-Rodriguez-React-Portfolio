17801aecddd97f2584f55a32684f6eb0f154889f
Sam Rodriguez <rodriguez_414@yahoo.com>
2021-02-11 18:50:32 -0800

    got the hamburger to work

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import "./style.css";
+import { Nav, NavItem, NavLink } from 'reactstrap';
 
 function Navbar() {
   const location = useLocation();
